19f61906421017ebb2800dc9de234015acc82ed9
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-21 17:14:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+
+
+// Express app
+const app = express();
+
+// Connect to DB and server listen
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => {
+    console.log('Connected to the database...');
+    app.listen(3000, () => console.log('Server is listening on port 3000...'));
+  })
+  .catch(error => console.log('Database connection error' + error));
+
+// Express session
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Set view engine
+app.set('views', './public/views');
+app.set('view engine', 'ejs');
+
+  app.get('/', (req, res) => {
+      res.send('Home');
+  }); 
