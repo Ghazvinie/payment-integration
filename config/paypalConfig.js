655e12de89e44ff55885e8b67967c963f199dbc1
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-22 17:23:39 +0100

    starting paypal config

--- config/paypalConfig.js
+++ config/paypalConfig.js
@@ -1,11 +1,16 @@
+const paypal = require('@paypal/checkout-server-sdk');
 const dotenv = require('dotenv').config();
-const paypalSdk = require('paypal-rest-sdk');
 
-paypalSdk.configure({
-    'host': 'api.sandbox.paypal.com',
-    'port': '',
-    'client_id': process.env.PAYPAL_CLIENT_ID,
-    'client_secret': process.env.PAYPAL_CLIENT_SECRET
-  });
+function environment() {
+  const clientId = process.env.PAYPAL_CLIENT_ID;
+  const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 
-  
+  return new paypal.core.SandboxEnvironment(
+    clientId, clientSecret
+  );
+
+}
+
+function client() {
+  return new paypal.core.PayPalHttpClient(environment());
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const paypal = require('@paypal/checkout-server-sdk');
 
 // Express app
 const app = express();
