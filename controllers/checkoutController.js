df7478ec9907d0467a5242382682e77c0053b886
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-29 17:42:54 +0100

    tidied up basket page

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -3,16 +3,13 @@ const dotenv = require('dotenv').config();
 function checkout(req, res) {
     const basket = req.body;
     req.session.basket = basket;
-    res.send({ redirect: '/purchase/checkout/basket' });
+
+    res.json({ redirect: '/purchase/checkout/basket' });
 }
 
 function basket(req, res) {
     const basket = req.session.basket;
-    if (!basket) {
-        res.redirect('/');
-    }
     res.render('basket', { basket: basket, message: 'Your Basket', key: process.env.STRIPE_PUBLISHABLE });
-    req.session.basket = null;
 }
 
 module.exports = { checkout, basket };
--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -4,15 +4,15 @@ const { createStripeItems } = require('../services/stripeServices');
 async function stripeCheckout(req, res) {
   const basket = req.session.basket;
   const itemsArray = createStripeItems(basket);
-  console.log(itemsArray)
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ['card'],
-//     line_items: itemsArray,
-//     mode: 'payment',
-//     success_url: `http://localhost:3000/paysuccess`,
-//     cancel_url: `http://localhost:3000/paycancel`
-//   });
-//   res.json({ id: session.id });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: itemsArray,
+    mode: 'payment',
+    success_url: `http://localhost:3000/paysuccess`,
+    cancel_url: `http://localhost:3000/paycancel`
+  });
+  console.log(session)
+  res.json({ id: session.id });
 }
 
 module.exports = { stripeCheckout };
