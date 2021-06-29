19282de066b3cf3979814fe48982eecdcb03767f
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-29 17:44:14 +0100

    tidied up basket page

--- server.js
+++ server.js
@@ -42,14 +42,6 @@ app.set('view engine', 'ejs');
 // Set static files
 app.use(express.static(__dirname + '/public'));
 
-app.all('*', (req, res, next) => {
-  if (!req.session.basket) {
-    res.redirect('/');
-  } else {
-    next();
-  }
-});
-
 app.get('/', (req, res) => {
   res.render('index');
 });
