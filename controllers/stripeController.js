a0fd5aa67900a1b3b37b5f11ab9edcadf691514a
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-02 09:11:26 +0100

    trying csp stuff

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,17 +2,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const { createStripeItems } = require('../services/stripeServices');
 
 async function stripeCheckout(req, res) {
+  console.log('hello')
   const basket = req.session.basket;
   const itemsArray = createStripeItems(basket);
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: itemsArray,
-    mode: 'payment',
-    success_url: `http://localhost:3000/paysuccess`,
-    cancel_url: `http://localhost:3000/paycancel`
-  });
-  console.log(session)
-  res.json({ id: session.id });
+  console.log(itemsArray[0].price_data.product_data)
+  // const session = await stripe.checkout.sessions.create({
+  //   payment_method_types: ['card'],
+  //   line_items: itemsArray,
+  //   mode: 'payment',
+  //   success_url: `http://localhost:3000/paysuccess`,
+  //   cancel_url: `http://localhost:3000/paycancel`
+  // });
+  // res.json({ id: session.id });
 }
 
 module.exports = { stripeCheckout };
