0f05416f6e8bc8c15d90a56684c8980b8bec17d6
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-05 16:52:31 +0100

    adding comments

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,9 +1,11 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const { createStripeItems } = require('../services/stripeServices');
 
+// Creates a Stripe session for user and executes the payment, sends back the session.id 
 async function stripeCheckout(req, res) {
   const basket = req.session.basket;
-  const itemsArray = createStripeItems(basket);
+  const itemsArray = createStripeItems(basket); // Creates an array of basket items for user with the .create() method
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: itemsArray,
