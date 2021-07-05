c142ea61cebfd7fdeca6d645b0bcdc12cc77caeb
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-05 16:44:39 +0100

    adding comments

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,22 +1,27 @@
 const dotenv = require('dotenv').config();
 
+// Saves basket to session and redirects user to the basket / payment method choice
 function checkout(req, res) {
     const basket = req.body;
     req.session.basket = basket;
     res.json({ redirect: '/purchase/checkout/basket' });
 }
 
+// Renders the basket page using basket saved in session
 function basket(req, res) {
     const basket = req.session.basket;
     res.render('basket', { basket: basket, message: 'Your Basket', key: process.env.STRIPE_PUBLISHABLE });
 }
 
+// Renders index page is user cancels checkout process
 function paymentCancel(req, res){
+    req.session.basket = null; // Set session basket to null
     res.render('index', { message: 'Payment cancelled' });
 }
 
+// Renders success page 
 function paymentSuccess(req, res){
-    req.session.basket = null;
+    req.session.basket = null; // Ser session basket to null
     res.render('success');
 }
 
