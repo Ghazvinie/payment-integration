7d7a47693f155ec68d04a00e2666024d7c96d2c3
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-05 17:58:44 +0100

    adding comments

--- services/stripeServices.js
+++ services/stripeServices.js
@@ -1,16 +1,17 @@
+// Creates an array of items formatted for Stripe checkout
 function createStripeItems(basket) {
   const items = Object.keys(basket);
   const itemsArray = items.reduce((acc, curr) => {
     if (curr !== 'delivery' && curr !== 'subTotal') {
       acc.push({
-        price_data: {
-          currency: 'gbp',
-          product_data: {
-            name: curr
+        'price_data': {
+          'currency': 'gbp',
+         ' product_data': {
+            'name': curr
           },
-          unit_amount: basket[curr].price * 100,
+          'unit_amount': basket[curr].price * 100,
         },
-        quantity: basket[curr].quantity,
+        'quantity': basket[curr].quantity,
       });
     }
     return acc;
