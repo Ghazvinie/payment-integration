50105282692b7fde569e7bde626602055ecd71c4
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-05 20:09:06 +0100

    updating stripe shipping

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -5,9 +5,13 @@ const { createStripeItems } = require('../services/stripeServices');
 async function stripeCheckout(req, res) {
   const basket = req.session.basket;
   const itemsArray = createStripeItems(basket); // Creates an array of basket items for use with the .create() method
-console.log('checkout')
+  const shippingRate = basket.delivery === 2.50 ? 'shr_1J9wvcInmADjXYb4zdqlHpQo' : 'shr_1J9wwQInmADjXYb4O0faeYDU';
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
+    shipping_rates: [shippingRate],
+    shipping_address_collection: {
+      allowed_countries: ['GB'],
+    },
     line_items: itemsArray,
     mode: 'payment',
     success_url: `http://localhost:3000/purchase/success`,
