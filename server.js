4288737194d2ff92b3d1de11e6892adfd394ab92
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-05 20:16:19 +0100

    some tidying

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ const purchaseRouter = require('./routers/purchaseRouter');
 const app = express();
 
 // Connect to DB and server listen
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then(() => {
-    console.log('Connected to the database...');
-    app.listen(3000, () => console.log('Server is listening on port 3000...'));
-  })
-  .catch(error => console.log('Database connection error' + error));
+app.listen(3000, () => console.log('Server is listening on port 3000...'));
 
 // PayPal configuration
 paypal.configure({
